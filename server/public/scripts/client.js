89a3ef4df7b261c3143bfceac1fb5e0ea867a742
Anthony Clarke <anthonydeandreclarke@gmail.com>
2024-11-15 20:48:09 -0600

    got the database to load when the page refreshes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,33 @@
 console.log('JS is sourced!');
+// calling everything in the  DB to be called when the page loads
+
+getTodos()
+
+function getTodos() { 
+    // get artist data from the server
+    axios({
+      method: 'GET',
+      url: '/todos'
+    }).then(function(response) {
+      console.log('Got artists', response.data);
+       renderTodos(response.data);
+    }).catch(function (error) {
+      console.log('error in artist get', error);
+    });
+}
+
+
+function renderTodos( listOfToDos ){
+const toDoTableBody = document.getElementById('toDoHome')
+
+toDoTableBody.innerHTML = '';
+
+for (let todo of listOfToDos){
+toDoTableBody.innerHTML += (`
+    <tr>
+        <td>${todo.text}</td>
+        <td>${todo.isComplete}</td>
+      </tr>
+    `)
+}
+}
